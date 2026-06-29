f92a42ca96dd27a30ff01024f94e454d9bd376ce
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:21:46 +0200

    1.1.8 Dodaj test przeciecia krzyzowego

---
+++ tests/LaserCad.Tests/Geometry/Intersections2DTests.cs
@@ -0,0 +1,25 @@
+using LaserCad.Geometry;
+
+namespace LaserCad.Tests.Geometry;
+
+public sealed class Intersections2DTests
+{
+    [Test]
+    public void IntersectSegments_CrossingSegments_ShouldReturnIntersectionPoint()
+    {
+        LineSegment2D horizontal = new LineSegment2D(
+            new Point2D(0.0, 5.0),
+            new Point2D(10.0, 5.0));
+        LineSegment2D vertical = new LineSegment2D(
+            new Point2D(4.0, 0.0),
+            new Point2D(4.0, 10.0));
+
+        IntersectionResult result = Intersections2D.Intersect(horizontal, vertical);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IsPoint, Is.True);
+            Assert.That(result.Point, Is.EqualTo(new Point2D(4.0, 5.0)));
+        });
+    }
+}
