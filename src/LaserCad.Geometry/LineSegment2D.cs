f1094e2794539e68f843a503dc2d7f48082491a2
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:14:16 +0200

    1.0.3 Dodaj koniec odcinka

--- src/LaserCad.Geometry/LineSegment2D.cs
+++ src/LaserCad.Geometry/LineSegment2D.cs
@@ -7,15 +7,21 @@ namespace LaserCad.Geometry;
 public readonly record struct LineSegment2D
 {
     /// <summary>
-    /// Tworzy zaczatek odcinka z punktem poczatkowym.
+    /// Tworzy odcinek pomiedzy punktem poczatkowym i koncowym.
     /// </summary>
-    public LineSegment2D(Point2D start)
+    public LineSegment2D(Point2D start, Point2D end)
     {
         Start = start;
+        End = end;
     }
 
     /// <summary>
     /// Punkt poczatkowy odcinka.
     /// </summary>
     public Point2D Start { get; }
+
+    /// <summary>
+    /// Punkt koncowy odcinka.
+    /// </summary>
+    public Point2D End { get; }
 }
