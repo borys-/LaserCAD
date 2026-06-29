c76729bd4ee26954cf7d968964cd16c6b42fd278
BorysPatyk <borys.patyk@gmail.com>
2026-06-30 01:04:39 +0200

    8.1.1 Wygeneruj przednia scianke

--- tests/LaserCad.Tests/Core/BoxGenerators/BoxGeneratorTests.cs
+++ tests/LaserCad.Tests/Core/BoxGenerators/BoxGeneratorTests.cs
@@ -1,5 +1,6 @@
 using LaserCad.Core.BoxGenerators;
 using LaserCad.Core.Documents;
+using LaserCad.Geometry;
 
 namespace LaserCad.Tests.Core.BoxGenerators;
 
@@ -13,5 +14,24 @@ public sealed class BoxGeneratorTests
         Sketch sketch = generator.GenerateSketch(new BoxGeneratorOptions());
 
         Assert.That(sketch.Name, Is.EqualTo("Box generator"));
+    }
+
+    [Test]
+    public void GenerateSketch_ShouldCreateFrontPanel()
+    {
+        var generator = new BoxGenerator();
+
+        Sketch sketch = generator.GenerateSketch(new BoxGeneratorOptions());
+
+        var frontPanel = (PolylineEntity)sketch.Entities[0];
+        Assert.That(frontPanel.Polyline.IsClosed, Is.True);
+        Assert.That(frontPanel.LayerName, Is.EqualTo(DefaultLayers.Cut.Name));
+        AssertPanelBounds(frontPanel.Bounds, expectedWidth: 106.0, expectedHeight: 56.0);
+    }
+
+    private static void AssertPanelBounds(BoundingBox bounds, double expectedWidth, double expectedHeight)
+    {
+        Assert.That(bounds.MaxX - bounds.MinX, Is.EqualTo(expectedWidth).Within(0.000001));
+        Assert.That(bounds.MaxY - bounds.MinY, Is.EqualTo(expectedHeight).Within(0.000001));
     }
 }
