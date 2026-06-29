34f204fef976d325e9e50dd54ef5288148618693
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:16:53 +0200

    1.0.11 Dodaj test transformacji odcinka

--- tests/LaserCad.Tests/Geometry/LineSegment2DTests.cs
+++ tests/LaserCad.Tests/Geometry/LineSegment2DTests.cs
@@ -39,4 +39,47 @@ public sealed class LineSegment2DTests
             Assert.That(segment.PointAt(1.0), Is.EqualTo(segment.End));
         });
     }
+
+    [Test]
+    public void Direction_ShouldReturnNormalizedVectorFromStartToEnd()
+    {
+        LineSegment2D segment = new LineSegment2D(
+            new Point2D(1.0, 2.0),
+            new Point2D(4.0, 6.0));
+
+        Vector2D direction = segment.Direction;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(direction.X, Is.EqualTo(0.6).Within(GeometryTolerance.Default));
+            Assert.That(direction.Y, Is.EqualTo(0.8).Within(GeometryTolerance.Default));
+        });
+    }
+
+    [Test]
+    public void Bounds_ShouldContainStartAndEnd()
+    {
+        LineSegment2D segment = new LineSegment2D(
+            new Point2D(4.0, -2.0),
+            new Point2D(-1.0, 6.0));
+
+        Assert.That(segment.Bounds, Is.EqualTo(new BoundingBox(-1.0, -2.0, 4.0, 6.0)));
+    }
+
+    [Test]
+    public void Transform_ShouldTransformStartAndEnd()
+    {
+        LineSegment2D segment = new LineSegment2D(
+            new Point2D(1.0, 2.0),
+            new Point2D(4.0, 6.0));
+        Matrix3x3 transform = Matrix3x3.CreateTranslation(10.0, -2.0) * Matrix3x3.CreateScaling(2.0, 3.0);
+
+        LineSegment2D result = segment.Transform(transform);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Start, Is.EqualTo(new Point2D(12.0, 4.0)));
+            Assert.That(result.End, Is.EqualTo(new Point2D(18.0, 16.0)));
+        });
+    }
 }
