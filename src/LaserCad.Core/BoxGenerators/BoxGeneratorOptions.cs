cc9d36fa6324508f11ab5a9185856e0b53717c0f
BorysPatyk <borys.patyk@gmail.com>
2026-06-30 00:16:40 +0200

    8.0.1 Dodaj szerokosc pudelka

--- src/LaserCad.Core/BoxGenerators/BoxGeneratorOptions.cs
+++ src/LaserCad.Core/BoxGenerators/BoxGeneratorOptions.cs
@@ -1,3 +1,5 @@
+using LaserCad.Geometry.Units;
+
 namespace LaserCad.Core.BoxGenerators;
 
 /// <summary>
@@ -5,4 +7,16 @@ namespace LaserCad.Core.BoxGenerators;
 /// </summary>
 public sealed class BoxGeneratorOptions
 {
+    /// <summary>
+    /// Tworzy opcje generatora pudelka z domyslnymi wymiarami startowymi.
+    /// </summary>
+    public BoxGeneratorOptions(Length? width = null)
+    {
+        Width = width ?? Length.FromMillimeters(100.0);
+    }
+
+    /// <summary>
+    /// Szerokosc pudelka mierzona w milimetrach.
+    /// </summary>
+    public Length Width { get; }
 }
