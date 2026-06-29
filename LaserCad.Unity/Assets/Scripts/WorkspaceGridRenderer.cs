8c3d5a0d609874d7ecc422dbe96229a5b6501ddc
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 15:17:51 +0200

    3.2.1 Dodaj siatke 1 mm

--- LaserCad.Unity/Assets/Scripts/WorkspaceGridRenderer.cs
+++ LaserCad.Unity/Assets/Scripts/WorkspaceGridRenderer.cs
@@ -18,7 +18,7 @@ namespace LaserCad.Unity
         private bool isVisible = true;
 
         [SerializeField]
-        private float gridExtentMillimeters = 500f;
+        private float minorStepMillimeters = 1f;
 
         [SerializeField]
         private Color lineColor = new Color(0.24f, 0.24f, 0.24f, 1f);
@@ -30,7 +30,7 @@ namespace LaserCad.Unity
 
         private void OnValidate()
         {
-            gridExtentMillimeters = Mathf.Max(1f, gridExtentMillimeters);
+            minorStepMillimeters = Mathf.Max(0.1f, minorStepMillimeters);
         }
 
         private void OnRenderObject()
@@ -41,11 +41,13 @@ namespace LaserCad.Unity
             }
 
             EnsureMaterial();
-            DrawGridBounds();
+            DrawMinorGrid();
         }
 
-        private void DrawGridBounds()
+        private void DrawMinorGrid()
         {
+            var bounds = GetVisibleBounds();
+
             lineMaterial.SetPass(0);
 
             GL.PushMatrix();
@@ -53,14 +55,45 @@ namespace LaserCad.Unity
             GL.Begin(GL.LINES);
             GL.Color(lineColor);
 
-            var extent = gridExtentMillimeters;
-            DrawLine(new Vector3(-extent, -extent, 0f), new Vector3(extent, -extent, 0f));
-            DrawLine(new Vector3(extent, -extent, 0f), new Vector3(extent, extent, 0f));
-            DrawLine(new Vector3(extent, extent, 0f), new Vector3(-extent, extent, 0f));
-            DrawLine(new Vector3(-extent, extent, 0f), new Vector3(-extent, -extent, 0f));
+            DrawVerticalLines(bounds, minorStepMillimeters);
+            DrawHorizontalLines(bounds, minorStepMillimeters);
 
             GL.End();
             GL.PopMatrix();
+        }
+
+        private Bounds2D GetVisibleBounds()
+        {
+            var min = workspaceCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f));
+            var max = workspaceCamera.ViewportToWorldPoint(new Vector3(1f, 1f, 0f));
+
+            return new Bounds2D(
+                Mathf.Min(min.x, max.x),
+                Mathf.Max(min.x, max.x),
+                Mathf.Min(min.y, max.y),
+                Mathf.Max(min.y, max.y));
+        }
+
+        private static void DrawVerticalLines(Bounds2D bounds, float step)
+        {
+            var start = Mathf.Floor(bounds.MinimumX / step) * step;
+            var end = Mathf.Ceil(bounds.MaximumX / step) * step;
+
+            for (var x = start; x <= end; x += step)
+            {
+                DrawLine(new Vector3(x, bounds.MinimumY, 0f), new Vector3(x, bounds.MaximumY, 0f));
+            }
+        }
+
+        private static void DrawHorizontalLines(Bounds2D bounds, float step)
+        {
+            var start = Mathf.Floor(bounds.MinimumY / step) * step;
+            var end = Mathf.Ceil(bounds.MaximumY / step) * step;
+
+            for (var y = start; y <= end; y += step)
+            {
+                DrawLine(new Vector3(bounds.MinimumX, y, 0f), new Vector3(bounds.MaximumX, y, 0f));
+            }
         }
 
         private static void DrawLine(Vector3 start, Vector3 end)
@@ -86,6 +119,25 @@ namespace LaserCad.Unity
             lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
             lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
             lineMaterial.SetInt("_ZWrite", 0);
+        }
+
+        private readonly struct Bounds2D
+        {
+            public Bounds2D(float minimumX, float maximumX, float minimumY, float maximumY)
+            {
+                MinimumX = minimumX;
+                MaximumX = maximumX;
+                MinimumY = minimumY;
+                MaximumY = maximumY;
+            }
+
+            public float MinimumX { get; }
+
+            public float MaximumX { get; }
+
+            public float MinimumY { get; }
+
+            public float MaximumY { get; }
         }
     }
 }
