e9a2c77c6cb7f94cf83a36b4013662c1713d611d
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:14:45 +0200

    1.0.5 Dodaj punkt posredni odcinka

--- src/LaserCad.Geometry/LineSegment2D.cs
+++ src/LaserCad.Geometry/LineSegment2D.cs
@@ -29,4 +29,20 @@ public readonly record struct LineSegment2D
     /// Dlugosc odcinka w milimetrach domenowych.
     /// </summary>
     public double Length => Start.DistanceTo(End);
+
+    /// <summary>
+    /// Zwraca punkt posredni dla parametru t z zakresu od 0 do 1.
+    /// Dla t = 0 zwraca poczatek, dla t = 1 zwraca koniec.
+    /// </summary>
+    public Point2D PointAt(double t)
+    {
+        if (t < 0.0 || t > 1.0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(t), "Parameter t must be between 0 and 1.");
+        }
+
+        return new Point2D(
+            Start.X + ((End.X - Start.X) * t),
+            Start.Y + ((End.Y - Start.Y) * t));
+    }
 }
