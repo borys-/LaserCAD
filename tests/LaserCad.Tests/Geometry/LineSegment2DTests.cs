fc8ae247a5509902786e9627f64671874b6be5b5
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:16:25 +0200

    1.0.10 Dodaj test punktu posredniego

--- tests/LaserCad.Tests/Geometry/LineSegment2DTests.cs
+++ tests/LaserCad.Tests/Geometry/LineSegment2DTests.cs
@@ -13,4 +13,30 @@ public sealed class LineSegment2DTests
 
         Assert.That(segment.Length, Is.EqualTo(5.0).Within(GeometryTolerance.Default));
     }
+
+    [Test]
+    public void PointAt_ShouldReturnInterpolatedPoint()
+    {
+        LineSegment2D segment = new LineSegment2D(
+            new Point2D(2.0, 4.0),
+            new Point2D(10.0, 8.0));
+
+        Point2D result = segment.PointAt(0.25);
+
+        Assert.That(result, Is.EqualTo(new Point2D(4.0, 5.0)));
+    }
+
+    [Test]
+    public void PointAt_ShouldReturnEndpointsForZeroAndOne()
+    {
+        LineSegment2D segment = new LineSegment2D(
+            new Point2D(2.0, 4.0),
+            new Point2D(10.0, 8.0));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(segment.PointAt(0.0), Is.EqualTo(segment.Start));
+            Assert.That(segment.PointAt(1.0), Is.EqualTo(segment.End));
+        });
+    }
 }
