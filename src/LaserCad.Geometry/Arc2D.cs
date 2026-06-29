968a75090a8d0ec2f6bfbc1fa5c0da5614de346c
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:28:35 +0200

    1.2.5 Utworz Arc2D

---
+++ src/LaserCad.Geometry/Arc2D.cs
@@ -0,0 +1,9 @@
+namespace LaserCad.Geometry;
+
+/// <summary>
+/// Luk okregu 2D.
+/// Uzywaj go dla fragmentow okregu w szkicach i algorytmach geometrii.
+/// </summary>
+public readonly record struct Arc2D
+{
+}
