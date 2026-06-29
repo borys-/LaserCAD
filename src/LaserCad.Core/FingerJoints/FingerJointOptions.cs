791e394ad1cd42da5eb913248a46f611edfe47f2
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 22:40:01 +0200

    7.0.0 Utworz FingerJointOptions

---
+++ src/LaserCad.Core/FingerJoints/FingerJointOptions.cs
@@ -0,0 +1,22 @@
+using LaserCad.Geometry.Units;
+
+namespace LaserCad.Core.FingerJoints;
+
+/// <summary>
+/// Opcje opisujace parametry polaczenia palcowego dla generatorow produkcyjnych.
+/// </summary>
+public sealed class FingerJointOptions
+{
+    /// <summary>
+    /// Tworzy opcje polaczenia palcowego z wartosciami domyslnymi bez kompensacji.
+    /// </summary>
+    public FingerJointOptions()
+    {
+        FingerWidth = Length.FromMillimeters(0.0);
+    }
+
+    /// <summary>
+    /// Docelowa szerokosc pojedynczego palca.
+    /// </summary>
+    public Length FingerWidth { get; }
+}
