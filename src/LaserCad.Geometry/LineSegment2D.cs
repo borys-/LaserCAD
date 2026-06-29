64405acce28b7b641b0c88be1dcd1ff53478117d
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:14:01 +0200

    1.0.2 Dodaj poczatek odcinka

--- src/LaserCad.Geometry/LineSegment2D.cs
+++ src/LaserCad.Geometry/LineSegment2D.cs
@@ -4,4 +4,18 @@ namespace LaserCad.Geometry;
 /// Odcinek 2D jako skonczony fragment prostej.
 /// Uzywaj go dla podstawowych encji liniowych w szkicach i algorytmach geometrii.
 /// </summary>
-public readonly record struct LineSegment2D;
+public readonly record struct LineSegment2D
+{
+    /// <summary>
+    /// Tworzy zaczatek odcinka z punktem poczatkowym.
+    /// </summary>
+    public LineSegment2D(Point2D start)
+    {
+        Start = start;
+    }
+
+    /// <summary>
+    /// Punkt poczatkowy odcinka.
+    /// </summary>
+    public Point2D Start { get; }
+}
