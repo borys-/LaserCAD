9e08f56a5e5f840bd74c3f5526a9c55fee9e5292
BorysPatyk <borys.patyk@gmail.com>
2026-06-29 08:13:46 +0200

    1.0.1 Dodaj LineSegment2D

---
+++ src/LaserCad.Geometry/LineSegment2D.cs
@@ -0,0 +1,7 @@
+namespace LaserCad.Geometry;
+
+/// <summary>
+/// Odcinek 2D jako skonczony fragment prostej.
+/// Uzywaj go dla podstawowych encji liniowych w szkicach i algorytmach geometrii.
+/// </summary>
+public readonly record struct LineSegment2D;
