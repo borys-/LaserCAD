5f9eef033142c0199664ca54121fb94258038cfa
BorysPatyk <borys.patyk@gmail.com>
2026-06-30 09:01:29 +0200

    3.6.7 Przenies generator pudelka do desktop shell

--- src/LaserCad.Desktop/MainWindow.xaml.cs
+++ src/LaserCad.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using LaserCad.Core.BoxGenerators;
+using LaserCad.Geometry.Units;
 
 namespace LaserCad.Desktop;
 
@@ -13,5 +15,45 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
+        RefreshDocumentSummary();
+    }
+
+    private void RebuildBoxButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var options = new BoxGeneratorOptions(
+                Length.FromMillimeters(ParseMillimeters(BoxWidthTextBox.Text, "Szerokosc")),
+                Length.FromMillimeters(ParseMillimeters(BoxDepthTextBox.Text, "Glebokosc")),
+                Length.FromMillimeters(ParseMillimeters(BoxHeightTextBox.Text, "Wysokosc")),
+                Length.FromMillimeters(ParseMillimeters(MaterialThicknessTextBox.Text, "Grubosc materialu")),
+                Length.FromMillimeters(ParseMillimeters(KerfTextBox.Text, "Kerf")),
+                Length.FromMillimeters(ParseMillimeters(FingerWidthTextBox.Text, "Szerokosc palca")),
+                Length.FromMillimeters(ParseMillimeters(ClearanceTextBox.Text, "Clearance")));
+
+            viewModel.ApplyBoxOptions(options);
+            RefreshDocumentSummary();
+        }
+        catch (Exception ex) when (ex is FormatException or ArgumentOutOfRangeException)
+        {
+            StatusTextBlock.Text = ex.Message;
+        }
+    }
+
+    private static double ParseMillimeters(string value, string fieldName)
+    {
+        if (!double.TryParse(value, out var result))
+        {
+            throw new FormatException("Niepoprawna wartosc pola: " + fieldName);
+        }
+
+        return result;
+    }
+
+    private void RefreshDocumentSummary()
+    {
+        DocumentNameTextBlock.Text = "Dokument: " + viewModel.CurrentDocument.Name;
+        SketchCountTextBlock.Text = "Szkice: " + viewModel.CurrentDocument.Sketches.Count;
+        StatusTextBlock.Text = viewModel.StatusText;
     }
 }
