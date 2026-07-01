1df7a54833e34d617178f412b15155adcfd22cf6
BorysPatyk <borys.patyk@gmail.com>
2026-07-01 09:04:07 +0200

    12.0.4 Utworz NestingOptions

---
+++ src/LaserCad.Core/Production/NestingOptions.cs
@@ -0,0 +1,14 @@
+namespace LaserCad.Core.Production;
+
+/// <summary>
+/// Opcje prostego nestingu elementow na arkuszu.
+/// </summary>
+public sealed class NestingOptions
+{
+    /// <summary>
+    /// Tworzy opcje nestingu.
+    /// </summary>
+    public NestingOptions()
+    {
+    }
+}
