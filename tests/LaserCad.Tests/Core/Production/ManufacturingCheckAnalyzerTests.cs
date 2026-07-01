820115bc113e46a115557f90546e63b2269ec9dd
BorysPatyk <borys.patyk@gmail.com>
2026-07-01 09:18:49 +0200

    12.1.8 Dodaj test otwartego konturu

--- tests/LaserCad.Tests/Core/Production/ManufacturingCheckAnalyzerTests.cs
+++ tests/LaserCad.Tests/Core/Production/ManufacturingCheckAnalyzerTests.cs
@@ -23,6 +23,25 @@ public class ManufacturingCheckAnalyzerTests
             check.EntityId == duplicate.Id));
     }
 
+    [Test]
+    public void Analyze_WithOpenPolyline_ShouldReturnOpenContourWarning()
+    {
+        var polyline = new PolylineEntity(new Polyline2D(new[]
+        {
+            new Point2D(0.0, 0.0),
+            new Point2D(10.0, 0.0),
+            new Point2D(10.0, 10.0),
+        }));
+        var document = CreateDocument(polyline);
+
+        var checks = new ManufacturingCheckAnalyzer().Analyze(document);
+
+        Assert.That(checks, Has.Some.Matches<ManufacturingCheck>(check =>
+            check.Code == "OpenContour" &&
+            check.Severity == ManufacturingCheckSeverity.Warning &&
+            check.EntityId == polyline.Id));
+    }
+
     private static CadDocument CreateDocument(params Entity[] entities)
     {
         return new CadDocument(sketches: new[] { new Sketch(entities: entities) });
